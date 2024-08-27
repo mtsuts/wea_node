b9ff274d740f6c61f132d33a0287b0f5b689c5fc
mtsuts <mariamtsuts@gmail.com>
2024-08-26 17:38:45 -0700

    update forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (lat, long, callback) => {
   const url = `https://api.weatherstack.com/current?access_key=fa9334ddac62182c3c0179a13f3cf1fd&query=${lat},${long}&units=m`
   request({ url, json: true }, (error, { body }) => {
@@ -8,7 +10,7 @@ const forecast = (lat, long, callback) => {
     } else if (body.error) {
       callback(`Unable to find location. Try another search`, undefined)
     } else {
-      callback(undefined, `Temperature is ${body.current.temperature}`)
+      callback(undefined, `Temperature is ${body.current.temperature}. It feels like ${body.current.feelslike}`)
     }
   })
 }
